26456627b461bc03bf885ba974728f026863a595
Twan Goosen <twan@clarin.eu>
2019-01-10 17:35:24 +0100

    creation of endpoints json

--- oai-endpoints-get-formats.sh
+++ oai-endpoints-get-formats.sh
@@ -13,14 +13,31 @@ ENDPOINT_INFO_URL="${API_URL}/model/OAIPMHEndpoint"
 # Main
 #########################################################################################
 main() {
-	get_endpoints
+	get_endpoints | {
+		while read ENDPOINT; do
+			get_formats_for_endpoint $ENDPOINT
+		done
+	} | jq -s # combine into a json array
 }
 
 #########################################################################################
 # Gets OAI-PMH endpoints from the centre registry
 #########################################################################################
 get_endpoints() {
-	curl -sL "${ENDPOINT_INFO_URL}"|jq '.[]|.fields.uri'
+	curl -sL "${ENDPOINT_INFO_URL}" | \
+		jq '.[]|.fields.uri' | \
+		sed -e 's/\"\(.*\)"$/\1/g'
+}
+
+#########################################################################################
+# Gets formats from the specified endpoint and turns this into a json object
+#########################################################################################
+get_formats_for_endpoint() {
+	ENDPOINT=$1
+	jq -n '{
+		uri: "'"${ENDPOINT}"'",
+		formats: ["",""]
+	}'
 }
 
 # Run main
